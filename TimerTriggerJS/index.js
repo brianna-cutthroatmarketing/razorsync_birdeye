140f48575549304c82868e7ef8ee622f4e0e3c4d
Brianna Wegryn <briannawegryn@gmail.com>
2018-09-22 16:35:36 -0400

    Parse work orders for customers that spent 00+

--- TimerTriggerJS/index.js
+++ TimerTriggerJS/index.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const asyncHandler = require('express-async-handler');
 const _ = require('underscore');
+//const { forEach } = require('p-iteration');
 
 let index = 10;
 
@@ -11,25 +12,44 @@ module.exports = async function (context, myTimer) {
     {
         context.log('JavaScript is running late!');
     }
-    await fullAutomation(context);
+    await fullAutomation200Plus(context);
     context.log('JavaScript timer trigger function ran!', timeStamp);   
 };
 
-const fullAutomation = asyncHandler(async(context) => {
+const fullAutomation200Plus = asyncHandler(async(context) => {
 	try {
-		let data = await gatherCustomerData(context);
-		await parseAndPublishData(context, data);
+		let work_orders = _.filter(await gatherWorkOrders(context), function(order) { return order.ServiceRequestId; });
+		let service_orders = {}, customer_orders = {};
+
+		await Promise.all( work_orders.map( async order => {
+			if (!service_orders[order.ServiceRequestId]) {
+				service_orders[order.ServiceRequestId] = await gatherServiceOrdersByID(context, order.ServiceRequestId);
+				if (!customer_orders[service_orders[order.ServiceRequestId].CustomerId]) { customer_orders[service_orders[order.ServiceRequestId].CustomerId] = []; }
+			}
+
+			let price = await gatherWorkOrderServiceItems(context, order);
+			customer_orders[service_orders[order.ServiceRequestId].CustomerId].push(price);
+		}));
+
+		await Promise.all( _.keys(customer_orders).map( async customer_id => {
+			let customer_spending = customer_orders[customer_id].reduce((a, b) => a + b, 0);
+			if (customer_spending > 200) {
+				let contact = await gatherSpecificCustomerData(context, customer_id);
+				await publishData(context, {name: `${contact.FirstName} ${contact.LastName}`, emailId: contact.Email, phone: contact.Phone, smsEnabled: (contact.NotifyViaSms ? 1 : 0), employees: []});
+			}
+		}));
 	} catch (error) {
 		context.error(`encountered error: ${JSON.stringify(error)}`);
 	}
 })
 
-const gatherCustomerData = (context) => {
+const gatherWorkOrders = (context) => {
 	let epochTime = (new Date).getTime();
+	context.log(`epochTime: ${epochTime}`);
 	return new Promise(function(resolve, reject) {
 		request({
 			method: 'POST',
-			url: "https://kayplumbingservices.0.razorsync.com/ApiService.svc/Customer/List",
+			url: "https://kayplumbingservices.0.razorsync.com/ApiService.svc/WorkOrder/List",
 			headers: {
 				"Token": "5a442567-97ee-4652-8f8a-0269b53b6b1e",
 				"Host": "kayplumbingservices.0.razorsync.com",
@@ -52,18 +72,74 @@ const gatherCustomerData = (context) => {
 	});
 }
 
-const parseAndPublishData = (context, customer_data) => {
-	_.each(customer_data, asyncHandler(async(customer) => {
-		if (customer.Contacts.length > 1) { context.log(`\ncustomer with multiple contacts ${JSON.stringify(customer)}\n`); }
-		try {
-			if (customer && customer.Contacts) {
-				let contact = customer.Contacts[customer.Contacts.length - 1];
-				await publishData(context, {name: `${contact.FirstName} ${contact.LastName}`, emailId: contact.Email, phone: contact.Phone, smsEnabled: (contact.NotifyViaSms ? 1 : 0), employees: []});
+const gatherServiceOrdersByID = (context, service_order_id) => {
+	return new Promise(function(resolve, reject) {
+		request({
+			method: 'GET',
+			url: `https://kayplumbingservices.0.razorsync.com/ApiService.svc/ServiceRequest/${service_order_id}`,
+			headers: {
+				"Token": "5a442567-97ee-4652-8f8a-0269b53b6b1e",
+				"Host": "kayplumbingservices.0.razorsync.com",
+				"ServerName": "kayplumbingservices",
+				"Content-Type": "application/json",
+		        "User-Agent": "node.js"
+			},
+			json: true
+		}, function(error, response) {
+			if (error || (response && response.statusCode != 200)) {
+				reject(error || response);
+			} else {
+				resolve(response.body);
 			}
-		} catch (error) {
-			context.error(`encountered error publishing to BirdEye: ${JSON.stringify(error)}`);
-		}
-	}));
+		});
+	});
+}
+
+const gatherWorkOrderServiceItems = (context, order) => {
+	return new Promise(function(resolve, reject) {
+		request({
+			method: 'GET',
+			url: `https://kayplumbingservices.0.razorsync.com/ApiService.svc/WorkOrderServiceItem/List/${order.Id}`,
+			headers: {
+				"Token": "5a442567-97ee-4652-8f8a-0269b53b6b1e",
+				"Host": "kayplumbingservices.0.razorsync.com",
+				"ServerName": "kayplumbingservices",
+				"Content-Type": "application/json",
+		        "User-Agent": "node.js"
+			},
+			json: true
+		}, function(error, response) {
+			if (error || (response && response.statusCode != 200)) {
+				reject(error || response);
+			} else {
+				resolve(_.reduce(_.pluck(response.body, 'CalculatedPrice'), function(memo, num){ return memo + num; }, 0));
+			}
+		});
+	});
+}
+
+const gatherSpecificCustomerData = (context, customer_id) => {
+	return new Promise(function(resolve, reject) {
+		request({
+			method: 'GET',
+			url: `https://kayplumbingservices.0.razorsync.com/ApiService.svc/Contact/List/${customer_id}`,
+			headers: {
+				"Token": "5a442567-97ee-4652-8f8a-0269b53b6b1e",
+				"Host": "kayplumbingservices.0.razorsync.com",
+				"ServerName": "kayplumbingservices",
+				"Content-Type": "application/json",
+		        "User-Agent": "node.js"
+			},
+			json: true
+		}, function(error, response) {
+			context.log(`res: ${JSON.stringify(response.body)}`);
+			if (error || (response && response.statusCode != 200)) {
+				reject(error || response);
+			} else {
+				resolve(response.body[response.body.length-1]);
+			}
+		});
+	});
 }
 
 const publishData = (context, customer) => {
@@ -77,7 +153,6 @@ const publishData = (context, customer) => {
 	context.log(`should now push customer: ${JSON.stringify(customer)}`);
 
 	return new Promise(function(resolve, reject) {
-		// resolve(true);
 		request({
 			method: 'POST',
 			url: "https://api.birdeye.com/resources/v1/customer/checkin",
@@ -101,5 +176,4 @@ const publishData = (context, customer) => {
 	});
 
 }
-
-//fullAutomation(console);
+fullAutomation200Plus(console);
